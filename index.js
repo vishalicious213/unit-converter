9d3d9c9e14a023afe14eca1c4d0eb4f74eb3a075
vishalicious213 <vishalicious213@gmail.com>
2023-01-05 14:59:42 -0800

    rendering feet to meters on click

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ digits.value = 0
 
 button.addEventListener("click", () => {
     let m2f = document.getElementById("m2f") // meters to feet
+    let f2m = document.getElementById("f2m") // feet to meters
     let l2g = document.getElementById("l2g") // liters to gallons
     let k2p = document.getElementById("k2p") // kilograms to pounds
     startingNumber.forEach(item => item.textContent = digits.value)
 
     m2f.textContent = (Number(digits.value) * 3.281).toFixed(2)
+    f2m.textContent = (Number(digits.value) * 0.304).toFixed(2)
     l2g.textContent = (Number(digits.value) * 0.264).toFixed(2)
     k2p.textContent = (Number(digits.value) * 2.204).toFixed(2)
 })
